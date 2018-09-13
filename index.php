689d9ab36d56ead7a3d5ed2a7311c33deeca9b6a
Antonio Sousa <tonysousantos@gmail.com>
2018-09-13 11:55:13 -0300

    Crud

--- index.php
+++ index.php
@@ -44,6 +44,70 @@ $app->get('/admin/logout',function(){
 	exit;
 });
 
+$app->get("/admin/users",function(){
+	User::verifyLogin();
+	$users = User::listAll();
+	$page = new PageAdmin();
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+});
+
+$app->get("/admin/users/create",function(){
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+	header("Location: /admin/users");
+});
+
+$app->get("/admin/users/:iduser",function($iduser){
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int) $iduser);
+
+	$page = new PageAdmin();
+	$page->setTpl("users-update", array(
+		"use"=>$user->getValues(),
+	));
+});
+
+$app->post("/admin/users/create", function(){
+	User::verifyLogin();
+	//var_dump($_POST);
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->setData($_POST);
+	$user->save();
+	//var_dump($user);
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+	User::verifyLogin();
+
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+
+
 $app->run();
 
 ?>
